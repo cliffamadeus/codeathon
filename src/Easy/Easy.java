4367511003b06b7fcfc87ec658ed3faea8ce38db
cliffamadeus <dev.clfm5t12@gmail.com>
2024-10-28 20:12:20 +0800

    Added solution for easy

---
+++ src/Easy/Easy.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class Easy {
+
+	public static void main(String[] args) {
+		int coda = 19;
+		int athon = 96;
+		
+		if(coda==19 && athon==96) {
+			System.out.println("Both condition are true");
+			System.out.println("Incremented coda: " + ++coda);
+			System.out.println("Incremented athon: " + ++athon);
+		}else {
+			System.out.println("False");
+		}
+
+	}
+
+}
