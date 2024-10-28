399028b854b4449a1322164bb2fb24263244ada6
cliffamadeus <dev.clfm5t12@gmail.com>
2024-10-28 20:22:13 +0800

    Added constructor for array on hard

--- src/Hard/Hard.java
+++ src/Hard/Hard.java
@@ -1,7 +1,14 @@
 package Hard;
+import java.util.Scanner;
 
 public class Hard {
+	private int[] myArray;
+	
+	public Hard(int size) {
+        myArray = new int[size];
+    }
 
+    
 	public static void main(String[] args) {
 		
 
