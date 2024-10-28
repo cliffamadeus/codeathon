f24f9276ded2b8117e57708201ee8e8e9c9029db
cliffamadeus <dev.clfm5t12@gmail.com>
2024-10-28 20:28:30 +0800

    Added total and avg expenses

--- src/Hard/Hard.java
+++ src/Hard/Hard.java
@@ -7,10 +7,42 @@ public class Hard {
 	public Hard(int size) {
         myArray = new int[size];
     }
-
+	
+	public void inputExpenses() {
+        Scanner scanner = new Scanner(System.in);
+      
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print("Enter expense for day " + (i + 1) + ": ");
+            myArray[i] = scanner.nextInt();
+        }
+    }
     
-	public static void main(String[] args) {
+	public void totalExpenses() {
+		int total = 0;
 		
+		for (int value: myArray) {
+			total +=value;
+		}
+		
+		System.out.println("The total of expenses is " + total );
+	}
+	
+	public void avgExpenses() {
+		int total = 0;
+		
+		for (int value: myArray) {
+			total +=value;
+		}
+		
+		System.out.println("The average of your expenses is " + total/myArray.length);
+	}
+	
+	public static void main(String[] args) {
+		Hard test = new Hard(5);
+		
+		test.inputExpenses();
+		test.totalExpenses();
+		test.avgExpenses();
 
 	}
 
