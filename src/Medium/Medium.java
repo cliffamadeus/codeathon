a32faca6cdacd1c989e19cefdad123d27102f5e3
cliffamadeus <dev.clfm5t12@gmail.com>
2024-10-28 20:19:42 +0800

    Added valid return condition for withdraw

--- src/Medium/Medium.java
+++ src/Medium/Medium.java
@@ -2,10 +2,16 @@ package Medium;
 
 public class Medium {
 	
-	double balance,amountToWithdraw;
+	double balance;
+	double amountToWithdraw;
 	
-	public double withdraw(double balance,double amountToWithdraw) {
-		return balance;
+	public double withdraw(double balance, double amountToWithdraw) {
+		
+		 if (balance > amountToWithdraw) {
+			 return balance - amountToWithdraw;
+	     }else {
+	    	 return balance;
+	     }
 	}
 
 	public static void main(String[] args) {
